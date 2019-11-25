761ea501717c8989e784359ba2911875bb9dab53
Eazzzy3 <blafubla@mail.ru>
2019-11-25 19:30:58 +0300

    лечу битб тебе ебало ой всм кушать

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -96,6 +96,8 @@ fun sibilants(inputName: String, outputName: String) {
             if (string[z] == 'Я') return 'А'
             if (string[z] == 'ю') return 'у'
             if (string[z] == 'Ю') return 'У'
+            if (string[z] == 'Ю') return 'У'
+
         }
         return string[z]
     }
